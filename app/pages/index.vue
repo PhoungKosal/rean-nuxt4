1ed4c6424a8938fefcc034c2951b366cc383ecfd
kosal <phoungkosal02@gmail.com>
2025-08-28 10:29:37 +0700

    feat: add Button component with variants and styles

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,4 @@
 <template>
   <h1 class="text-3xl font-bold bg-blue-500">Index page</h1>
+  <Button>Click me</Button>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  srcDir: 'app/',
+  alias: {
+    '#shared': './shared'
+  },
 
   modules: [
     '@nuxt/eslint',
@@ -10,7 +14,8 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
-    '@nuxt/test-utils'
+    '@nuxt/test-utils',
+    'shadcn-nuxt',
   ],
 
   css: ['~/assets/css/main.css'],
@@ -19,5 +24,17 @@ export default defineNuxtConfig({
     plugins: [
       tailwindcss(),
     ],
+  },
+
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "app/components/ui"
+     */
+    componentDir: 'app/components/ui'
   },
 })
